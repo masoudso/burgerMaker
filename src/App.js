0acbc1f946c26750e04334902783b4dc11c31cbd
masoudso <veisi.masoud@gmail.com>
2018-05-08 22:18:47 -0400

    new font added and was referenced in index.html and used in index.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Layout from './Components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <Layout>
+          <p>Let's see if it works..</p>
+        </Layout>
       </div>
     );
   }
