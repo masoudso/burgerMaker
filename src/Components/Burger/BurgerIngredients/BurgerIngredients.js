b7aa3987e04be2f3392a6a8514e4e9d57ea68344
masoudso <veisi.masoud@gmail.com>
2018-05-09 14:08:18 -0400

    css file for burger ingredients is added and is referenced using a switch case in burgeringredients.js

---
+++ src/Components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import classes from './BurgerIngredients.css'
+
+const burgerIngredients = (props) => {
+    let ingredients = null;
+
+    switch (props.type) {
+        case 'bread-bottom':
+            ingredients = <div className={classes.Breadbottom}></div>
+            break;
+            case 'bread-top':
+            ingredients = (
+            <div className={classes.BreadTop}>
+                <div className={classes.Seeds1}></div>
+                <div className={classes.Seeds2}></div>
+            </div>
+            );
+            break;
+            case 'meat':
+            ingredients = <div className={classes.Meat}></div>
+            break;
+            case 'cheese':
+            ingredients = <div className={classes.Cheese}></div>
+            break;
+            case 'bacon':
+            ingredients = <div className={classes.Bacon}></div>
+            break;
+            case 'salad':
+            ingredients = <div className={classes.Salad}></div>
+            break;
+    
+        default:
+            ingredients = null;
+    }
+    return ingredients;
+}
+
+export default burgerIngredients;
