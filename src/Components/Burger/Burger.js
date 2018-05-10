4020dcc9057b62238006e21c7f65777490d3a6e6
masoudso <veisi.masoud@gmail.com>
2018-05-10 16:45:38 -0400

    BurgerJs has a dynamic way of taking ingredients in - burgerBuilder is now stateful

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -2,14 +2,23 @@ import React from 'react'
 import classes from './Burger.css'
 import BurgerIngredients from './BurgerIngredients/BurgerIngredients'
 
+/* A Short note about function below
+   Object.Keys receives the ingredients as an object and returns the keys (here 
+   they are Salad, Meat, ..., then the function makes a new array using spread
+    operator for each key with size of each key (number of meat, cheese, etc)
+    and again maps it using index and a unique key.)     
+*/
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => { 
+            return <BurgerIngredients key={igKey + i} type={igKey} />;
+        }); 
+    });
+
     return(
         <div className={classes.Burger}>
         <BurgerIngredients type="bread-top"/>
-        <BurgerIngredients type="cheese"/>
-        <BurgerIngredients type="meat"/>
-        <BurgerIngredients type="cheese"/>
-
+        {transformedIngredients} 
         <BurgerIngredients type="bread-bottom"/>
         </div>
     );
