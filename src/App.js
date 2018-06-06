345de61c087bd003428f08fea05b2389673fcc5d
masoudso <veisi.masoud@gmail.com>
2018-06-06 10:12:55 -0400

    initial commit for react-routing

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './Containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './Containers/Checkout/Checkout'
+import {Route , Switch} from 'react-router-dom'
+// adding Switch is just for having alternate ways routing
+// Switch tells the router to choose the first route that matches'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
-          <Checkout />
+          <Route path="/" exact component={BurgerBuilder}/>
+          <Route path="/Checkout" component={Checkout}/>
+          {/* <BurgerBuilder />
+          <Checkout /> */}
         </Layout>
       </div>
     );
