0ae99f11363285834c3425e51d458851a0b5587f
masoudso <veisi.masoud@gmail.com>
2018-06-10 10:52:20 -0400

    orders page is complete, it now fetched data from the backend

--- src/Components/Order/Order.js
+++ src/Components/Order/Order.js
@@ -1,11 +1,35 @@
 import React from 'react'
 import classes from './Order.css'
 
-const order = (props) => (
+const order = (props) => {
+
+    const ingredients1 = []
+
+    for (let stuff in props.ingredients){
+        ingredients1.push({
+            name: stuff, amount: props.ingredients[stuff]
+        })
+    }
+
+    const ingredients2 = ingredients1.map(ig => {
+        return <span
+            style={{
+                textTransform: 'capitalize',
+                display: 'inline-block',
+                margin: '0 8px',
+                border: '1px solid #ccc',
+                padding: '5px'
+            }}
+            key={ig.name}>
+            {ig.name}:({ig.amount})
+        </span>
+    })
+    return(
     <div className={classes.Order}>
-        <p>Ingredients: Lettuce (1)</p>
-        <p>Price<strong>: 10 USD</strong></p>
+        <p>{ingredients2} </p>
+        <p>Price<strong>: ${props.price}</strong></p>
         </div>
-)
+    )
+}
 
 export default order;
--- src/Containers/Orders/Orders.js
+++ src/Containers/Orders/Orders.js
@@ -1,15 +1,46 @@
 import React, {Component} from 'react'
 import Order from '../../Components/Order/Order'
+import axios from '../../axios-orders'
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
 
 class Orders extends Component{
+    state = {
+        orders : [],
+        loading : true
+    }
+    
+    componentDidMount(){
+        axios.get('/orders.json')
+        .then(res => { 
+            const fetchedOrders = [];
+            for (let key in res.data){/* 1 */
+                fetchedOrders.push({...res.data[key], id: key})
+            }
+            this.setState({loading : false, orders: fetchedOrders})
+        })
+        .catch(err => {
+            this.setState({loading : false})
+        })
+    }
+
     render(){
         return(
             <div>
-                <Order />
-                <Order />
+                {this.state.orders.map(order => {
+                     return (<Order 
+                     key={order.id}
+                     ingredients={order.ingredients}
+                     price = {order.price}
+                     />)
+                })}
             </div>
         )
     }
 }
 
-export default Orders;
+export default  withErrorHandler(Orders, axios); 
+
+/* 1: Since firebase is returning a json object, we need to turn it into an array,
+and for that, we loop through each key (a unique key that can be found in console.log)
+and push the keys into an empty array that we defined earlier.
+Also, the reason for having ...res.data[key] is to create a new array*/
